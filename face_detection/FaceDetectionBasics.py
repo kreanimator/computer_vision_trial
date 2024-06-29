f46aebd4ca566a441f607694d57dad7c60ef379c
Kreanimator <backin.v@yandex.ru>
2024-06-29 14:19:36 +0300

    Added mediapipe recognition to face detection

--- face_detection/FaceDetectionBasics.py
+++ face_detection/FaceDetectionBasics.py
@@ -5,13 +5,20 @@ import time
 cap = cv2.VideoCapture(0)
 p_time = 0
 
+mp_face_detection = mp.solutions.face_detection
+mp_draw = mp.solutions.drawing_utils
+face_detection = mp_face_detection.FaceDetection()
+
 while True:
     success, img = cap.read()
-    cv2.imshow("Image", img)
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = face_detection.process(img_rgb)
+
     c_time = time.time()
-    fps = 1/(c_time/p_time)
+    fps = 1 / (c_time - p_time)
     p_time = c_time
     cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
-                3, (255, 0, 0), 2)
-
+                3, (0, 255, 0), 2)
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
