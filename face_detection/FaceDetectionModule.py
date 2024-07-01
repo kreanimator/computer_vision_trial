22a629df6d5cecbee98d13040a4f8b81b9a6e6e3
Kreanimator <backin.v@yandex.ru>
2024-07-01 11:56:50 +0300

    Finished basic prototype of the face detection module

--- face_detection/FaceDetectionModule.py
+++ face_detection/FaceDetectionModule.py
@@ -4,39 +4,42 @@ import cv2
 import mediapipe as mp
 import time
 
+
 class FaceDetector:
-    def __init__(self, min_detection_con = 0.5):
+    def __init__(self, min_detection_con=0.5):
 
         self.min_detection_con = min_detection_con
         self.mp_face_detection = mp.solutions.face_detection
         self.mp_draw = mp.solutions.drawing_utils
         self.face_detection = self.mp_face_detection.FaceDetection()
 
+    def find_faces(self, img, draw=True):
 
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.face_detection.process(img_rgb)
+        bboxs = []
 
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = face_detection.process(img_rgb)
-
-    if results.detections:
-        for id, detection in enumerate(results.detections):
-            # print(id, detection)
-            # mp_draw.draw_detection(img, detection)
-            bbox_c = detection.location_data.relative_bounding_box
-            ih, iw, ic = img.shape
-            bbox = int(bbox_c.xmin * iw), int(bbox_c.ymin * ih), \
-                int(bbox_c.width * iw), int(bbox_c.height * ih)
-
-            cv2.rectangle(img, bbox, (255, 0, 255), 2)
-            cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN,
-                        2, (255, 0, 255), 2)
-
+        if self.results.detections:
+            for id, detection in enumerate(self.results.detections):
+                bbox_c = detection.location_data.relative_bounding_box
+                ih, iw, ic = img.shape
+                bbox = int(bbox_c.xmin * iw), int(bbox_c.ymin * ih), \
+                    int(bbox_c.width * iw), int(bbox_c.height * ih)
+                bboxs.append([id, bbox, detection.score])
+                cv2.rectangle(img, bbox, (255, 0, 255), 2)
+                cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN,
+                            2, (255, 0, 255), 2)
+        return img, bboxs
 
 
 def main():
     cap = cv2.VideoCapture(0)
     p_time = 0
+    detector = FaceDetector()
     while True:
         success, img = cap.read()
+
+        img, bboxs = detector.find_faces(img)
 
         c_time = time.time()
         fps = 1 / (c_time - p_time)
@@ -52,6 +55,7 @@ def main():
         # Check if the window is closed by looking if any windows are still open
         if cv2.getWindowProperty("Image", cv2.WND_PROP_VISIBLE) < 1:
             break
+
 
 if __name__ == "__main__":
     main()
