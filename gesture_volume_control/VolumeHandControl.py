29f0a8dc67093c27c81b04e510859f84d959f8c6
Kreanimator <backin.v@yandex.ru>
2024-07-02 15:16:09 +0300

    Basic project setup

--- gesture_volume_control/VolumeHandControl.py
+++ gesture_volume_control/VolumeHandControl.py
@@ -1,1 +1,26 @@
 __author__ = "Valentin Bakin"
+
+import cv2
+import time
+import numpy as np
+
+
+w_cam, h_cam = 1280, 720
+
+cap = cv2.VideoCapture(0)
+cap.set(3, w_cam)
+cap.set(4, h_cam)
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+
+    # Break the loop if 'Esc' key is pressed
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+    # Check if the window is closed by looking if any windows are still open
+    if cv2.getWindowProperty("Image", cv2.WND_PROP_VISIBLE) < 1:
+        break
