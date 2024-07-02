46ae3fde510307885f5c53e7a4ae6ad760d62b98
Kreanimator <backin.v@yandex.ru>
2024-07-02 16:09:25 +0300

    Set basic volume control

--- gesture_volume_control/VolumeHandControl.py
+++ gesture_volume_control/VolumeHandControl.py
@@ -2,9 +2,13 @@ __author__ = "Valentin Bakin"
 
 import cv2
 import time
+import math
 import numpy as np
 
 from hand_tracking import HandTrackingModule as htm
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 w_cam, h_cam = 1280, 720
 
@@ -15,19 +19,47 @@ p_time = 0
 
 detector = htm.HandDetector(detection_confidence=0.7)
 
+#  Audio init
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None
+)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volume_range = volume.GetVolumeRange()
+
+min_vol = volume_range[0]
+max_vol = volume_range[1]
+
 while True:
     success, img = cap.read()
     img = detector.find_hands(img)
     lm_list = detector.find_position(img, draw=False)
     if len(lm_list) != 0:
-        print(lm_list[4], lm_list[8])
+        # print(lm_list[4], lm_list[8])
         x1, y1 = lm_list[4][1], lm_list[4][2]  # Thumb
         x2, y2 = lm_list[8][1], lm_list[8][2]  # Index finger
 
-        cx, cy = (x1 + x2) / 2, (y1 + y2) / 2  # Center of the line
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2  # Center of the line
         cv2.circle(img, (x1, y1), 15, (0, 255, 0), cv2.FILLED)
         cv2.circle(img, (x2, y2), 15, (0, 255, 0), cv2.FILLED)
         cv2.line(img, (x1, y1), (x2, y2), (200, 255, 0), 3)
+        cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        # print(length)
+
+        #  Hand range 30 -> 210
+        #  Volume range -65 -> 0
+
+        vol = np.interp(length, [30, 210], [min_vol, max_vol])
+        print(volume)
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length < 30:
+            cv2.circle(img, (cx, cy), 10, (0, 0, 255), cv2.FILLED)
 
     c_time = time.time()
     fps = 1 / (c_time - p_time)
