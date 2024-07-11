a828889de97f4e095f84e7d02eb9a68e88a6c765
Kreanimator <backin.v@yandex.ru>
2024-07-11 14:44:22 +0300

    Added find_distance function

--- hand_tracking/HandTrackingModule.py
+++ hand_tracking/HandTrackingModule.py
@@ -1,6 +1,6 @@
 __author__ = "Valentin Bakin"
 
-
+import math
 from typing import List
 import cv2
 import mediapipe as mp
@@ -70,13 +70,28 @@ class HandDetector:
         else:
             fingers.append(0)
         # Rest fingers (only for right hand for now)
-        for f_id in range(1,5):
-            if self.lm_list[self.tip_ids[f_id]][2] < self.lm_list[self.tip_ids[f_id]-2][2]:
+        for f_id in range(1, 5):
+            if self.lm_list[self.tip_ids[f_id]][2] < self.lm_list[self.tip_ids[f_id] - 2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
 
         return fingers
+
+    def find_distance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lm_list[p1][1:]
+        x2, y2 = self.lm_list[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
 
 def main():
     # Initialize variables for calculating FPS
